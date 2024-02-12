3422eef68467fbced1f984dd65a9b9210e6809a0
Andrey <dron2323.and@yandex.ru>
2024-02-12 18:46:56 +0300

    fearst commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let div = document.createElement('div');
+div.className = 'field';
+div.style.background = '#000';
+div.style.width = '600px';
+div.style.height = '500px';
+div.style.margin = '0 auto';
+document.body.style = '#1f1f1f'
+document.body.append(div);
