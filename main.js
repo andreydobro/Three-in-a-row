3173c4538fe5bfd6666b8aa4ecd939ac26244184
Andrey <dron2323.and@yandex.ru>
2024-02-14 20:31:14 +0300

    The container block has been added and styles

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Конфигурация игры
-let  = {
+let config = {
 
-    containerColorBg: '#A2A2A2',
+    // containerColorBg: ('#00C1FE', '#0375FA'),
+    containerColorBg: `linear-gradient(45deg, #00C1FE, #0375FA)`,
     contentColorBg: '#D9D9D9',
 
     rows: 6,
@@ -37,6 +38,8 @@ let components = {
 }
 
 
+initGame();
+
 function initGame() {
     document.body.style.margin = '0px';
     createPage();
@@ -44,5 +47,15 @@ function initGame() {
     createWraper();
     createCursor();
     createScore();
+}
 
+function createPage() {
+    components.container.style.background = config.containerColorBg;
+    components.container.style.height = '100vh';
+    components.container.style.overflow = 'hiden';
+    components.container.style.display = 'flex';
+    components.container.style.alignItems = 'center';
+    components.container.style.justifyContent = 'center';
+
+    document.body.append(components.container);
 }
